e258a9988719b0905221e1cf3ccebd40320c6192
Yab1 <yabilisanu@gmail.com>
2023-02-23 17:29:53 +0300

    favicon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Likes {likes}</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
